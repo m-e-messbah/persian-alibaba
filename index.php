a6bed0c3793f5c594464391d477f934053c9b177
messbah <eng.aramezani@gmail.com>
2019-12-18 23:24:14 +0330

    initial commit

---
+++ index.php
@@ -0,0 +1,364 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<meta name="description" content="فروشگاه علی بابا" />
+	<meta name="keywords" content="" />
+	<link rel="shortcut icon" href="image/icon/header/female-hair-shape-and-face-silhouette.png" />
+	<title>persian alibaba</title>
+
+	<link href="bootstrap-4.4.1-dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/default/default.css" media="all" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+        
+    <link rel="stylesheet" href="css\pages\index.css" media="all" />
+	
+	<script src="js/jquery-3.1.1.min.js"></script>
+</head>
+<body>
+    <header>
+	</header>
+    <div id="wrraper">
+        <!-- banner -->
+        <div class="banner paral marg no-padding">
+            <div class="part-1 no-padding">
+                <div>
+                    <div class="menu-header">
+                        <i class="material-icons">menu</i>
+                        <h3>دسته بندی های محبوب</h3>
+                    </div>
+                    <ul>
+                        <a href="#"><li>محصولات الکترونیک<i class="material-icons">fiber_manual_record</i></li></a>
+                        <a href="#"><li>پوشاک<i class="material-icons">fiber_manual_record</i></li></a>
+                        <a href="#"><li>لوازم منزل<i class="material-icons">fiber_manual_record</i></li></a>
+                        <a href="#"><li>آرایشی بهداشتی<i class="material-icons">fiber_manual_record</i></li></a>
+                        <a href="#"><li>محصولات الکترونیک<i class="material-icons">fiber_manual_record</i></li></a>
+                        <a href="#"><li>پوشاک<i class="material-icons">fiber_manual_record</i></li></a>
+                        <a href="#"><li>لوازم منزل<i class="material-icons">fiber_manual_record</i></li></a>
+                        <a href="#"><li>آرایشی بهداشتی<i class="material-icons">fiber_manual_record</i></li></a>
+                    </ul>
+                    <a href="#" id="all">همه محصولات</a> 
+                </div>
+            </div>
+            <div class="part-2 carousel slide no-padding-right" id="carousel">
+                <ul class="carousel-indicators">
+                    <li data-target="#carousel" data-slide-to="0" class="active"></li>
+                    <li data-target="#carousel" data-slide-to="1"></li>
+                    <li data-target="#carousel" data-slide-to="2"></li>
+                    <li data-target="#carousel" data-slide-to="3"></li>
+                    <li data-target="#carousel" data-slide-to="4"></li>
+                    <li data-target="#carousel" data-slide-to="5"></li>
+                </ul>
+                
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="image/post/carousel.jpg" alt="">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="image/post/carousel-2.jpg" alt="">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="image/post/carousel-3.jpg" alt="">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="image/post/carousel-4.jpg" alt="">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="image/post/carousel-2.jpg" alt="">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="image/post/carousel-3.jpg" alt="">
+                    </div>
+                </div>
+                <a class="carousel-control-prev" href="#carousel" data-slide="prev">
+                    <span class="carousel-control-prev-icon"></span>
+                </a>
+                <a class="carousel-control-next" href="#carousel" data-slide="next">
+                    <span class="carousel-control-next-icon"></span>
+                </a>
+            </div>
+            <div class="part-3 no-padding-left">
+                <h3>پیشنهاد هفته</h3>
+                <div class="week-prod-list">
+                    <div class="prod position">
+                        <h4>ایرپاد اپل</h4>
+                        <img src="image/post/product.jpg" alt="">
+                        <a href=""><button>خرید کنید</button></a>
+                    </div>
+                    <div class="border"></div>
+                    <div class="prod position">
+                        <h4>ایرپاد اپل</h4>
+                        <img src="image/post/product.jpg" alt="">
+                        <a href=""><button>خرید کنید</button></a>
+                    </div>
+                    <div class="border"></div>
+                    <div class="prod position">
+                        <h4>ایرپاد اپل</h4>
+                        <img src="image/post/product.jpg" alt="">
+                        <a href=""><button>خرید کنید</button></a>
+                    </div>
+                </div>
+            </div>
+            <div class="clearfix"></div>
+        </div>
+
+        <div class="clearfix"></div>
+        <!-- first row -->
+        <div class="first-row paral marg">
+            <div class="part position">
+                <h3>لوازم جانبی خودرو</h3>
+                <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز </p>
+                <img src="image/post/car.jpg" alt="" class="img-header">
+                <div class="clearfix"></div>
+                <div class="box">
+                    <div class="box-prod">
+                        <h4>لورم ایپسوم</h4>
+                        <div class="img-box">
+                            <img src="image/post/box-img.jpg" alt="">
+                            <img src="image/post/box-img.jpg" alt="">
+                        </div>
+                    </div>
+                    <div class="box-prod">
+                        <h4>لورم ایپسوم</h4>
+                        <div class="img-box">
+                            <img src="image/post/box-img.jpg" alt="">
+                            <img src="image/post/box-img.jpg" alt="">
+                        </div>
+                    </div>
+                    <div class="box-prod">
+                        <h4>لورم ایپسوم</h4>
+                        <div class="img-box">
+                            <img src="image/post/box-img.jpg" alt="">
+                            <img src="image/post/box-img.jpg" alt="">
+                        </div>
+                    </div>
+                  
+                </div>
+            </div>
+            <div class="part position">
+                <h3>لوازم جانبی خودرو</h3>
+                <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز </p>
+                <img src="image/post/car.jpg" alt="" class="img-header">
+                <div class="clearfix"></div>
+                <div class="box">
+                    <div class="box-prod">
+                        <h4>لورم ایپسوم</h4>
+                        <div class="img-box">
+                            <img src="image/post/box-img.jpg" alt="">
+                            <img src="image/post/box-img.jpg" alt="">
+                        </div>
+                    </div>
+                    <div class="box-prod">
+                        <h4>لورم ایپسوم</h4>
+                        <div class="img-box">
+                            <img src="image/post/box-img.jpg" alt="">
+                            <img src="image/post/box-img.jpg" alt="">
+                        </div>
+                    </div>
+                    <div class="box-prod">
+                        <h4>لورم ایپسوم</h4>
+                        <div class="img-box">
+                            <img src="image/post/box-img.jpg" alt="">
+                            <img src="image/post/box-img.jpg" alt="">
+                        </div>
+                    </div>
+                  
+                </div>
+            </div>
+        </div> 
+        <div class="clearfix"></div>
+        <!-- second row -->
+       <div class="second-row paral marg position">
+            <h3 id="head">لورم ایپسوم</h3>
+            <div class="box">
+                <div class="part" style="background: url(image/post/second-row.jpg)">
+                    <div class="content">
+                        <h4>بهترین محصولات این ماه</h4>
+                        <button>مشاهده</button>
+                    </div>
+                </div>
+                <div class="part">
+                    <div class="sale-box">
+                        <ul class="ul-sale-box">
+                            <li>
+                                <a href="">
+                                    <h3>لورم ایپسوم</h3>
+                                    <img src="image/post/sale-box.jpg" alt="">
+                                    <p>لورم ایپسوم</p>
+                                    <p>لورم ایپسوم</p>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="">
+                                    <h3>لورم ایپسوم</h3>
+                                    <img src="image/post/sale-box.jpg" alt="">
+                                    <p>لورم ایپسوم</p>
+                                    <p>لورم ایپسوم</p>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="">
+                                    <h3>لورم ایپسوم</h3>
+                                    <img src="image/post/sale-box.jpg" alt="">
+                                    <p>لورم ایپسوم</p>
+                                    <p>لورم ایپسوم</p>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="">
+                                    <h3>لورم ایپسوم</h3>
+                                    <img src="image/post/sale-box.jpg" alt="">
+                                    <p>لورم ایپسوم</p>
+                                    <p>لورم ایپسوم</p>
+                                </a>
+                            </li>
+                        </ul>
+                        <div class="border"></div>
+                        <div class="clearfix"></div>
+                        <ul class="ul-sale-box">
+                            <li>
+                                <a href="">
+                                    <h3>لورم ایپسوم</h3>
+                                    <img src="image/post/sale-box.jpg" alt="">
+                                    <p>لورم ایپسوم</p>
+                                    <p>لورم ایپسوم</p>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="">
+                                    <h3>لورم ایپسوم</h3>
+                                    <img src="image/post/sale-box.jpg" alt="">
+                                    <p>لورم ایپسوم</p>
+                                    <p>لورم ایپسوم</p>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="">
+                                    <h3>لورم ایپسوم</h3>
+                                    <img src="image/post/sale-box.jpg" alt="">
+                                    <p>لورم ایپسوم</p>
+                                    <p>لورم ایپسوم</p>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="">
+                                    <h3>لورم ایپسوم</h3>
+                                    <img src="image/post/sale-box.jpg" alt="">
+                                    <p>لورم ایپسوم</p>
+                                    <p>لورم ایپسوم</p>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+       </div>
+       <div class="clearfix"></div>
+       <div class="second-row paral marg position">
+            <h3 id="head">لورم ایپسوم</h3>
+            <div class="box">
+                <div class="part" style="background: url(image/post/second-row-1.jpg)">
+                    <div class="content">
+                        <h4>بهترین محصولات این ماه</h4>
+                        <button>مشاهده</button>
+                    </div>
+                </div>
+                <div class="part">
+                    <div class="sale-box">
+                        <ul class="ul-sale-box">
+                            <li>
+                                <a href="">
+                                    <h3>لورم ایپسوم</h3>
+                                    <img src="image/post/sale-box.jpg" alt="">
+                                    <p>لورم ایپسوم</p>
+                                    <p>لورم ایپسوم</p>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="">
+                                    <h3>لورم ایپسوم</h3>
+                                    <img src="image/post/sale-box.jpg" alt="">
+                                    <p>لورم ایپسوم</p>
+                                    <p>لورم ایپسوم</p>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="">
+                                    <h3>لورم ایپسوم</h3>
+                                    <img src="image/post/sale-box.jpg" alt="">
+                                    <p>لورم ایپسوم</p>
+                                    <p>لورم ایپسوم</p>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="">
+                                    <h3>لورم ایپسوم</h3>
+                                    <img src="image/post/sale-box.jpg" alt="">
+                                    <p>لورم ایپسوم</p>
+                                    <p>لورم ایپسوم</p>
+                                </a>
+                            </li>
+                        </ul>
+                        <div class="border"></div>
+                        <div class="clearfix"></div>
+                        <ul class="ul-sale-box">
+                            <li>
+                                <a href="">
+                                    <h3>لورم ایپسوم</h3>
+                                    <img src="image/post/sale-box.jpg" alt="">
+                                    <p>لورم ایپسوم</p>
+                                    <p>لورم ایپسوم</p>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="">
+                                    <h3>لورم ایپسوم</h3>
+                                    <img src="image/post/sale-box.jpg" alt="">
+                                    <p>لورم ایپسوم</p>
+                                    <p>لورم ایپسوم</p>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="">
+                                    <h3>لورم ایپسوم</h3>
+                                    <img src="image/post/sale-box.jpg" alt="">
+                                    <p>لورم ایپسوم</p>
+                                    <p>لورم ایپسوم</p>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="">
+                                    <h3>لورم ایپسوم</h3>
+                                    <img src="image/post/sale-box.jpg" alt="">
+                                    <p>لورم ایپسوم</p>
+                                    <p>لورم ایپسوم</p>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+       </div>
+       <div class="clearfix"></div>
+       <!-- third row with timer -->
+       <div class="third-row paral marg position">
+
+       </div>
+    </div>
+
+    <footer>
+	</footer>
+	
+</body>
+</html>
+
+
+<script src="bootstrap-4.4.1-dist/js/bootstrap.min.js"></script>
+<script>
+
+</script>
